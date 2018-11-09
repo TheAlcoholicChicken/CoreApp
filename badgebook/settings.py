a57e28d9e35070931e788e004bb8b13b8ce97bff
yuqingqiu <yuqingqiu93@gmail.com>
2018-11-09 15:26:49 -0800

    Refactored the project name to badgebook

--- badgebook/settings.py
+++ badgebook/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'loginpage.urls'
+ROOT_URLCONF = 'badgebook.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'loginpage.wsgi.application'
+WSGI_APPLICATION = 'badgebook.wsgi.application'
 
 
 # Database
