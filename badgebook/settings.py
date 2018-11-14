d1069182c6786cf9dec6071c993f3372012f1b59
yuqingqiu <yuqingqiu93@gmail.com>
2018-11-14 10:03:19 -0800

    Setting tweak on static

--- badgebook/settings.py
+++ badgebook/settings.py
@@ -121,4 +121,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/public'), )
-print(STATICFILES_DIRS)
+# print(STATICFILES_DIRS)
