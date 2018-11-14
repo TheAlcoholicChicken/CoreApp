737023fe3f33ce40aecee6a0f5bd3a552a035654
yuqingqiu <yuqingqiu93@gmail.com>
2018-11-14 11:49:34 -0800

    API resolve first step, get user id

--- login/views.py
+++ login/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def api(request):
+    reqpath = str(request.path)
+    user_id = reqpath.split('/')[2]
+    print(user_id)
+    return JsonResponse({'foo': 'bar'})
