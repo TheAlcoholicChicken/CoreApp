913dc35227da58d4c016bc4adf9d925a35fa6e8c
Tony Lin <tonylin514@gmail.com>
2018-11-14 10:07:08 -0800

    Added Users Collections model to sqllite. Commands:
    python manage.py makemigrations
    python manage.py migrate

--- login/models.py
+++ login/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class UsersCollection(models.Model):
+    user_id = models.CharField(max_length=100)
+    user_profile_link = models.URLField()  # first_name + last_name(camelcase)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    profile_picture_url = models.URLField()  # ImageField? or standard char/text field
+    description = models.TextField()
+
+    def __str__(self):
+        return self.user_id
