3173a586f0da0ef7fd905f7df61bf624864aac16
yuqingqiu <yuqingqiu93@gmail.com>
2018-11-13 22:53:21 -0800

    static file folder set to frontend/public

--- badgebook/urls.py
+++ badgebook/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'admin/', admin.site.urls),
     url(r'login/', include('login.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
