0dc4862ddd9f1a45f9b0d2c3d1be72efb3029d61
Tony Lin <tonylin514@gmail.com>
2018-11-16 15:11:04 -0800

    Implemented simple API service to add users to DB. Currently takes userid, firstname, lastname.

--- login/forms.py
+++ login/forms.py
@@ -2,3 +2,9 @@ from django import forms
 
 class UserForm(forms.Form):
     search = forms.CharField()
+
+
+class AddUserForm(forms.Form):
+    userid = forms.CharField()
+    firstname = forms.CharField()
+    lastname = forms.CharField()
