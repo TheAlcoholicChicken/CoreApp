8be6dcd55fc862a5bc7741bd18bd56e888fce429
Arlios <luyen_mai@live.com>
2018-11-23 13:37:06 -0800

    Axios calls implementation,

--- frontend/src/components/user_descripion.js
+++ frontend/src/components/user_descripion.js
@@ -10,20 +10,25 @@ class user_description extends Component {
             firstName : '',
             lastName : '',
             profilePicture : '',
-            userDescription : ''
+            userDescription : '',
+            userId : ''
         }
 
     }
 
+
     componentDidMount() {
         axios.get('user/something').then(
             response => this.setState({
+                userId : response.data.user_id,
                 firstName : response.data.first_name,
                 lastName : response.data.last_name,
                 profilePicture : response.data.profile_picture,
                 userDescription : response.data.description
             })
-        )
+        ).catch(error => {
+            console.log(error);
+        });
     }
 
     render() {
