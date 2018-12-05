a2386db1290bbb06534d2c3b80be0c5eccd0f8ee
teehuang <tonyhuang1996@hotmail.ca>
2018-12-05 03:42:38 -0800

    re added create account

---
+++ frontend/src/components/create_account.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import '../styles/login.css'
+import Popup from 'reactjs-popup'
+
+
+class create_account extends Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch('/user/login/', {
+      method: 'POST',
+      body: data,
+    });
+  }
+
+  render() {
+      // return (
+      //   <form onSubmit={this.handleSubmit}>
+      //     <label htmlFor="email">Enter your email</label>
+      //     <input id="email" name="email" type="email" />
+      //
+      //     <label htmlFor="password">Enter password</label>
+      //     <input id="password" name="password" type="text" required/>
+      //
+      //     <label htmlFor="password">Please confirm password</label>
+      //     <input id="password" name="confirmPassword" type="text" required/>
+      //
+      //     <button>Create Account!</button>
+      //   </form>
+      // );
+      return (
+        <Popup
+          trigger={<button className="button"> Create Account! </button>}
+          modal
+          closeOnDocumentClick
+        >
+
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="email">Enter your email</label>
+            <input id="email" name="email" type="email" />
+
+            <label htmlFor="password">Enter password</label>
+            <input id="password" name="password" type="text" required/>
+
+            <label htmlFor="password">Please confirm password</label>
+            <input id="password" name="confirmPassword" type="text" required/>
+
+            <button>Create Account!</button>
+          </form>
+
+        </Popup>
+      );
+  }
+}
+
+export default create_account;
