fa44a21007f1989822007554d38ed2907faeaa49
Tony Lin <tonylin514@gmail.com>
2018-12-04 23:58:32 -0800

    Updated login methods and some get user methods. Landing page now loads: /user/user_id_stuff_here/ Model now returns proper responses for get users and search users

--- login/urls.py
+++ login/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.login_page, name="login_page"), # Get page
     re_path(r'^user/profile/\w+', views.profile_page), # Get page
-    re_path(r'^user/\w+(?!/)$', views.landing_page), # Get page, user/123
-    re_path(r'^user/get_badges/\w+', views.getUserBadges, name='getBadges'), # Post only
+    re_path(r'^user/get_badges/', views.getUserBadges, name='getBadges'), # Post only
     re_path(r'^user/create_account/', views.createAccount, name="createAccount"), # Post only
-    re_path(r'^user/search_user/', views.getUser, name='searchUser'), # Post only
+    re_path(r'^user/search_user/', views.searchUser, name='searchUser'), # Post only
     re_path(r'^user/login/', views.login, name='login'), # Post only
+    re_path(r'^user/\w+', views.landing_page), # Get page, user/123
     re_path(r'^user/', views.getUser, name='user'), # Post only user/123/
 ]
--- login/views.py
+++ login/views.py
@@ -20,25 +20,24 @@ __BADGE_URL = 'https://management-system-api.herokuapp.com/user/get_badges/'
 def login_page(request):
     return render(request, 'login.html')
 
+
 # returns profile page for http get requests
 def profile_page(request):
     return render(request, 'profile.html')
 
+
 # returns landing page for http get requests
 def landing_page(request):
-    return render(request, 'landing.html')
+    return render(request, 'index.html')
 
 
-#TODO Implement login request and response
 @csrf_exempt
 def login(request):
     print(str(request.path))
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
+    if request.method == 'POST':
         form = F.LoginUserForm(request.POST)
         if form.is_valid():
-            print('login|form.cleaned_data',form.cleaned_data)
+            print('login|form.cleaned_data', form.cleaned_data)
             form = form.cleaned_data
             response = requests.post(__LOGIN_URL,
                                      data={'user_email': form['email'],
@@ -46,9 +45,16 @@ def login(request):
                                            'token': __TOKEN})
             if response.status_code == 400:
                 return JsonResponse({'response': response.json()['msg']})
-            else:
+            elif response.status_code == 200 and response.json()['user_id'] != '':
                 print('login|response.json()', response.json()['user_id'])
-                return render(request, 'index.html')
+                msg = response.json()['msg']
+                user_id = response.json()['user_id']
+                url = 'user/' + user_id + '/'
+                return JsonResponse({'response': msg, 'user_id': user_id, 'url': url})
+            else:
+                msg = 'Management API not up or no response'
+                print('login|', msg)
+                return JsonResponse({'response': msg})
         else:
             return JsonResponse({'response': 'invalid form'})
     else:
@@ -59,15 +65,24 @@ def login(request):
 @csrf_exempt
 def getUser(request):
     print(str(request.path))
-    if request.method == 'GET':
-        userid = request.path.split('/')[-1]
-        print('getUser|user_id', userid)
-        return JsonResponse(UsersCollection.get_user_json(userid))
-    elif request.method == 'POST':
+    if request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         print('getUser|request.body:', data)
-        result = '\n'.join(UsersCollection.search_user(data['search']))
-        if result == '':
+        userid = data['user_id']
+        print('getUser|user_id', userid)
+        return JsonResponse(UsersCollection.get_user_json(userid))
+    else:
+        return JsonResponse({'response': 'invalid form'})
+
+@csrf_exempt
+def searchUser(request):
+    print(str(request.path))
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        print('searchUser|request.body:', data)
+        id = data['id_only'] if 'id_only' in data else True
+        result = UsersCollection.search_user(data['search'], id)
+        if result['users'] == []:
             return JsonResponse({'response': 'no users'})
         else:
             print(result)
@@ -76,20 +91,19 @@ def getUser(request):
         return JsonResponse({'response': 'invalid form'})
 
 
-#TODO try catch block incase maanagement goes offline
+#TODO Managemet needs to follow their API protocols and wrap response under "badges" key
 @csrf_exempt
 def getUserBadges(request):
     print(str(request.path))
-    if request.method == 'GET':
-        userid = request.path.split('/')[-3]
-        print('getUserBadges|',userid)
-        try:
-            response = requests.post(__BADGE_URL, data={'user_id':userid,'token':__TOKEN})
-            print('getUserBadges|response.json()',response.json())
-            return JsonResponse(response.json())
-        except:
-            return JsonResponse({'response': 'Not implemented'})
-    return JsonResponse({'response': 'Not implemented'})
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        userid = data['user_id']
+        print('getUserBadges|user_id', userid)
+        response = requests.post(__BADGE_URL, data={'user_id': userid, 'token': __TOKEN})
+        print('getUserBadges|response.json()', response.json())
+        return JsonResponse({'badges': response.json()})
+    else:
+        return JsonResponse({'response': 'invalid form'})
 
 
 @csrf_exempt
