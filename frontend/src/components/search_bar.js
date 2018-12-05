57eed2795f73acc0709a2b8bd990a169bfc72419
Arlios <luyen_mai@live.com>
2018-12-05 15:13:42 -0800

    Search bar early implementation.

--- frontend/src/components/search_bar.js
+++ frontend/src/components/search_bar.js
@@ -1,22 +1,34 @@
 import React, {Component} from 'react';
 import '../styles/styles.css'
 import axios from 'axios'
-import {SearchInput} from 'evergreen-ui'
+import {Combobox} from 'evergreen-ui'
 class search_bar extends Component {
     constructor() {
       super();
       // this.handleSubmit = this.handleSubmit.bind(this);
       this.state = {
-          searchQuery : ''
+          searchQuery : 'tony',
+          searchItems : []
       }
     }
 
     handleSubmit(event) {
         alert(this.state.searchQuery);
         console.log(this.state.searchQuery);
-
         let data = this.state.searchQuery;
         axios.post('/user/', {'search' : data}).then(response => this.setState({data: response.response}))
+    }
+
+
+    componentDidMount() {
+        axios.post('/user/search_user/',  {
+            search: this.state.searchQuery
+        }).then(
+            response => {
+                this.setState({searchItems: response.data.users})
+            }
+        );
+        console.log(this.state.searchItems);
     }
 
     render() {
@@ -24,11 +36,14 @@ class search_bar extends Component {
             <div className="search-bar">
               <form className="example">
                 {/*<input id='search' value={this.state.searchQuery} type="text" placeholder="Search.." name="search" onChange={this.setQuery}/>*/}
-                <SearchInput
-                    name={'text-input-name'}
-                    placeholder={'Search for Users'}
-                    onChange={e => this.setState({searchQuery: e.target.value})}
-                    value={this.state.value}
+                <Combobox
+                  items={this.state.searchItems}
+                  onChange={e=> this.setState({searchItems: e.target.value})}
+                  placeholder="Users"
+                  autocompleteProps={{
+                    // Used for the title in the autocomplete.
+                    title: 'Users'
+                  }}
                 />
                 <button type={'submit'}><i className="fa fa-search"></i></button>
               </form>
