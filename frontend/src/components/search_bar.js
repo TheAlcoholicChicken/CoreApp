9afd1a5628095e2d436e7162a07fd275c611da44
Arlios <luyen_mai@live.com>
2018-12-05 16:42:18 -0800

    Implemented early search bar functionality

--- frontend/src/components/search_bar.js
+++ frontend/src/components/search_bar.js
@@ -7,39 +7,38 @@ class search_bar extends Component {
       super();
       // this.handleSubmit = this.handleSubmit.bind(this);
       this.state = {
-          searchQuery : 'tony',
+          searchQuery : ' ',
           searchItems : []
       }
     }
 
-    handleSubmit(event) {
-        alert(this.state.searchQuery);
-        console.log(this.state.searchQuery);
-        let data = this.state.searchQuery;
-        axios.post('/user/', {'search' : data}).then(res => this.setState({data: res.users}))
-    }
-
-
     componentDidMount() {
         axios.post('/user/search_user/',  {
-            search: this.state.searchQuery
+            search: this.state.searchQuery,
+            id_only: false
         }).then(
             response => {
-                this.setState({searchItems: response.users})
+                // console.log("Search response: " + response.data);
+                this.setState({searchItems: response.data.users})
             }
         );
-        console.log(this.state.searchItems);
     }
 
     render() {
         let searchDropdownItems = this.state.searchItems;
+        let searchNames = [];
+        console.log(searchDropdownItems);
+        searchDropdownItems.forEach((value, key)=>{
+            searchNames.push(searchDropdownItems[key].data.first_name);
+        });
+
         return (
             <div className="search-bar">
               <form className="example">
                 {/*<input id='search' value={this.state.searchQuery} type="text" placeholder="Search.." name="search" onChange={this.setQuery}/>*/}
                 <Combobox
-                  items={searchDropdownItems}
-                  onChange={e=> this.setState({searchItems: e.target.value})}
+                  items={searchNames}
+                  onChange={e=> console.log(e)}
                   placeholder="Users"
                   autocompleteProps={{
                     // Used for the title in the autocomplete.
